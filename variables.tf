80614934bc167d40e59c1702654735e59f94f03b
unknown <harha@gmail.com>
2025-08-19 15:00:43 +0530

    cicd infra

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "harshavardhan.site"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z08522663KW6H1TJ65HHE"
+  description = "description"
+}
