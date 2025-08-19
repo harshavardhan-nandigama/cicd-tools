ef3c74dbfc440938929f99bf66a8f93d7589bcbb
unknown <harha@gmail.com>
2025-08-20 01:15:33 +0530

    cicd

--- jenkins-agent.sh
+++ jenkins-agent.sh
@@ -31,10 +31,13 @@ systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
 
-curl -O curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+#kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mv kubectl /usr/local/bin/kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+kubectl version
 
+#helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
